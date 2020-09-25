2a15d28e18becffae702de800aea0e1bd31db2b2
Mark <mark.binlab@gmail.com>
2020-09-25 18:28:30 +0300

    Fix an error with finding not selected AMI

--- ami.tf
+++ ami.tf
@@ -1,5 +1,5 @@
 data aws_ami "coreos" {
-  count = var.ami_image != "" ? 0 : 1
+  count = var.ami_image == "" && var.ami_vendor == "coreos" ? 1 : 0
 
   most_recent = true
   owners      = ["595879546273"]
@@ -23,7 +23,7 @@ data aws_ami "coreos" {
 }
 
 data aws_ami "flatcar" {
-  count = var.ami_image != "" ? 0 : 1
+  count = var.ami_image == "" && var.ami_vendor == "flatcar" ? 1 : 0
 
   most_recent = true
   owners      = ["075585003325"]
