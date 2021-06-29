90600048e73126375fb48c4ce1c8d195c56da971
Nikita Velgin <direct.vegas@gmail.com>
2021-06-29 22:38:09 +0300

    Bump providers versions
    Required to use terraform version 1.0

--- versions.tf
+++ versions.tf
@@ -1,9 +1,23 @@
 terraform {
   required_version = ">= 0.12"
+
   required_providers {
-    aws      = ">= 2.53.0"
-    tls      = ">= 2.1.1"
-    ignition = ">= 1.2.1"
-    local    = ">= 1.4.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    tls = {
+      version = "~> 3.1"
+    }
+    local = {
+      version = "~> 2.1"
+    }
+    ignition = {
+      source  = "community-terraform-providers/ignition"
+      version = "1.3.0"
+      # Ignition version 0.34 supported by flatcar
+      # https://registry.terraform.io/providers/community-terraform-providers/ignition/latest/docs#ignition-versions
+      # https://kinvolk.io/flatcar-container-linux/releases/
+    }
   }
 }
