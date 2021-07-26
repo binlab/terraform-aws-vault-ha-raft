b0a81b3a78a64ec4ce5663125b89dd976649ad2e
Mark <mark.binlab@gmail.com>
2021-07-27 01:36:18 +0300

    Update example Assigning Vault cluster to external VPC

--- examples/assign-external-vpc/variables.tf
+++ examples/assign-external-vpc/variables.tf
@@ -5,3 +5,11 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "aws_profile" {
+  description = <<-EOT
+    AWS Profile
+  EOT
+  type        = string
+  default     = "default"
+}
